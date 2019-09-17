104e419882fae77040fc82f44ddc8f87a84c4c09
Aurel Feer <aurel.feer@gmail.com>
2019-09-17 14:29:44 +0200

    added admin dashboard route

--- imports/api/vueRoutes.js
+++ imports/api/vueRoutes.js
@@ -3,6 +3,7 @@ import Demo from '/imports/ui/main.demo.vue';
 import GameList from '/imports/ui/lobby.gamelist.vue';
 import EditGame from '/imports/ui/game.edit.vue';
 import PlayGame from '/imports/ui/game.play.vue';
+import AdminDashboard from '/imports/ui/admin.dashboard.vue';
 
 export default {
 	router: new VueRouter({
@@ -11,7 +12,8 @@ export default {
 			{ path: '/', name: 'root', component: Demo },
 			{ path: '/games/', name: 'games', component: GameList },
 			{ path: '/games/:gameid/edit', name: 'games.edit', component: EditGame },
-			{ path: '/games/:gameid/play', name: 'games.play', component: PlayGame }
+			{ path: '/games/:gameid/play', name: 'games.play', component: PlayGame },
+			{ path: '/admin', name: 'admin.dashboard', component: AdminDashboard }
 		]
 	})
 }
---
+++ imports/ui/admin.dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="admin-dashboard">
+		
+	</div>
+</template>
+<script>
+
+export default {
+	components: {
+	},
+	watch: {
+	},
+	meteor: {
+		$subscribe: {
+
+		},
+	}
+}
+</script>
