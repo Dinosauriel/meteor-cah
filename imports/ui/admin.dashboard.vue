748ea97b7ed6156a9a31e5ef776931c944efb6e1
Aurel Feer <aurel.feer@gmail.com>
2019-09-19 15:22:02 +0200

    added reimport Cardsets button

--- imports/ui/admin.dashboard.vue
+++ imports/ui/admin.dashboard.vue
@@ -1,6 +1,12 @@
 <template>
 	<div class="admin-dashboard">
-		
+		<h1>Admin Dashboard</h1>
+		<div class="card">
+			<div class="card-body">
+				<h5 class="card-title">Cardsets</h5>
+				<button type="button" class="btn btn-primary" v-on:click="reimportCardsets">Reimport Cardsets</button>
+			</div>
+		</div>
 	</div>
 </template>
 <script>
@@ -9,6 +15,11 @@ export default {
 	components: {
 	},
 	watch: {
+	},
+	methods: {
+		reimportCardsets() {
+			Meteor.call('reimportCardsets');
+		}
 	},
 	meteor: {
 		$subscribe: {
--- imports/ui/main.navbar.vue
+++ imports/ui/main.navbar.vue
@@ -1,5 +1,5 @@
 <template>
-	<nav class="navbar navbar-dark bg-dark">
+	<nav class="navbar navbar-dark bg-dark mb-3">
         <span class="navbar-brand">Cards Against Humanity</span>
         <span class="navbar-text"><span class="badge badge-danger mr-3" v-if="!isConnected">Disconnected</span>{{ username }}</span>
     </nav>
