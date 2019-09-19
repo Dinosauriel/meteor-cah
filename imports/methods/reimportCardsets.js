9c238297e8854d96fd4278663abf509580b43235
Aurel Feer <aurel.feer@gmail.com>
2019-09-19 14:38:52 +0200

    implemented reimportCardsets method

--- imports/methods/reimportCardsets.js
+++ imports/methods/reimportCardsets.js
@@ -1,5 +1,14 @@
 Meteor.methods({
 	reimportCardsets() {
 		console.log('reimporting cardsets');
+		Cardsets.remove({});
+
+		var json = Assets.getText('resources/cards/basekit.json');
+		var obj = JSON.parse(json);
+
+		for (cardset in obj.cardsets) {
+			Cardsets.insert(cardset);
+		}
+		console.log('imported ' + Cardsets.find({}).count() + ' cardsets.');
 	},
 });
