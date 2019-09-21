7f3526abe7f7d7f26d9f75e0861740cbe99e3746
Aurel Feer <aurel.feer@gmail.com>
2019-09-21 21:32:18 +0200

    improved cardsets manager layout

--- imports/ui/admin.cardsets.manager.vue
+++ imports/ui/admin.cardsets.manager.vue
@@ -1,11 +1,16 @@
 <template>
-	<div class="card">
-		<div class="card-body">
-			<h5 class="card-title">Cardsets</h5>
-			<button type="button" class="btn btn-primary" v-on:click="reimportCardsets">Reimport Cardsets</button>
-		</div>
-		<div class="card-footer text-muted">
-			There are {{numberOfCardsets}} Cardsets
+	<div class="col-sm-6">
+		<div class="card">
+			<div class="card-body">
+				<h5 class="card-title">Cardsets</h5>
+				<p class="card-text">Removes all Cardsets from the Database and reimports them from the JSON files:</p>
+				<p>
+					<code>/private/resources/cards/basekit.json</code>
+					<code>/private/resources/cards/customkit.json</code>
+				</p>
+				<button type="button" class="btn btn-primary" v-on:click="reimportCardsets">Reimport Cardsets</button>
+				<p class="card-text"><small class="text-muted">There are {{numberOfCardsets}} cardsets imported</small></p>
+			</div>
 		</div>
 	</div>
 </template>
