7b350938b6ecaaceb41a329fdd496ad727ef9fca
Aurel Feer <aurel.feer@gmail.com>
2019-09-21 16:41:25 +0200

    added cardsets manager component

---
+++ imports/ui/admin.cardsets.manager.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="card">
+		<div class="card-body">
+			<h5 class="card-title">Cardsets</h5>
+			<button type="button" class="btn btn-primary" v-on:click="reimportCardsets">Reimport Cardsets</button>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	methods: {
+		reimportCardsets() {
+			Meteor.call('reimportCardsets');
+		}
+	},
+}
+</script>
--- imports/ui/admin.dashboard.vue
+++ imports/ui/admin.dashboard.vue
@@ -1,25 +1,19 @@
 <template>
 	<div class="admin-dashboard">
 		<h1>Admin Dashboard</h1>
-		<div class="card">
-			<div class="card-body">
-				<h5 class="card-title">Cardsets</h5>
-				<button type="button" class="btn btn-primary" v-on:click="reimportCardsets">Reimport Cardsets</button>
-			</div>
-		</div>
+		<cardsetsmanager></cardsetsmanager>
 	</div>
 </template>
 <script>
+import cardsetsmanager from '/imports/ui/admin.cardsets.manager.vue';
 
 export default {
 	components: {
+		cardsetsmanager
 	},
 	watch: {
 	},
 	methods: {
-		reimportCardsets() {
-			Meteor.call('reimportCardsets');
-		}
 	},
 	meteor: {
 		$subscribe: {
